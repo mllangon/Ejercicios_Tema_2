354c2248aec795400baad75e2cd3e06a4c7a7151
mllangon <mllangon@myuax.com>
2024-05-02 20:26:35 +0200

    6

--- src/main/java/_2_/Reloj.java
+++ src/main/java/_2_/Reloj.java
@@ -10,7 +10,7 @@ public class Reloj {
         timer = new Timer();
     }
 
-    public void iniciarAlarma(Lampara lampara) {
+    public void iniciarAlarma(final Lampara lampara) {
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
@@ -20,9 +20,8 @@ public class Reloj {
         System.out.println("La alarma del reloj se ha configurado para encender la lámpara en 5 segundos.");
     }
 
-    protected void finalize() throws Throwable {
+    public void cancelarAlarma() {
         timer.cancel();
         System.out.println("El reloj está siendo desechado y su timer cancelado.");
-        super.finalize();
     }
 }
